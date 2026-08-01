429234155e8c7ff446d3a4f528ad552a4f003dd8
boranthegreat <mustafabboran@gmail.com>
2026-08-02 02:51:27 +0300

    Add files via upload

--- components/PushNotificationControl.tsx
+++ components/PushNotificationControl.tsx
@@ -8,7 +8,7 @@ function decodeKey(value: string) {
   const padding = "=".repeat((4 - (value.length % 4)) % 4);
   const base64 = (value + padding).replace(/-/g, "+").replace(/_/g, "/");
   const raw = window.atob(base64);
-  return Uint8Array.from([...raw].map((character) => character.charCodeAt(0)));
+  return Uint8Array.from(raw, (character) => character.charCodeAt(0));
 }
 
 export function PushNotificationControl() {
